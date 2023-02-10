f6d82ec4d56e6672ddf60c7371531cce9292661a
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-10 22:09:29 +0900

    feat: 데스크테리어 게시판 게시글 모두 불러오기 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  /* 데스크테리어 게시글 모두 불러오기 */
+  @Get('/showmethedesk')
+  getAllDeskPosts() {
+    return this.boardsService.getAllDeskPosts();
+  }
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private deskPosts: object[] = [
+    {
+      id: 1,
+      username: 'user1',
+      pic: 'picture1',
+    },
+    {
+      id: 2,
+      username: 'user2',
+      pic: 'picture2',
+    },
+    {
+      id: 3,
+      username: 'user3',
+      pic: 'picture3',
+    },
+  ];
+
+  getAllDeskPosts() {
+    return this.deskPosts;
+  }
+}
