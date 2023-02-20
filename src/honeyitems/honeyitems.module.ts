48010db2215c2fdca0bb330cfc789ff8dfc31a58
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-20 13:47:02 +0900

    feat: typeorm, multer 모듈 추가

--- src/honeyitems/honeyitems.module.ts
+++ src/honeyitems/honeyitems.module.ts
@@ -1,9 +1,32 @@
+import { HoneyItem } from 'src/honeyitems/honeyitem.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { HoneyitemsController } from './honeyitems.controller';
 import { HoneyitemsService } from './honeyitems.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([HoneyItem]),
+    MulterModule.register({
+      dest: './upload',
+      limits: {
+        fileSize: 52428800,
+      },
+      fileFilter: (req, file, cb) => {
+        const ext = file.mimetype;
+        if (
+          ext === 'image/jpeg' ||
+          ext === 'image/jpg' ||
+          ext === 'image/png'
+        ) {
+          return cb(null, true);
+        }
+        return cb(null, false);
+      },
+    }),
+  ],
   controllers: [HoneyitemsController],
-  providers: [HoneyitemsService]
+  providers: [HoneyitemsService],
 })
 export class HoneyitemsModule {}
