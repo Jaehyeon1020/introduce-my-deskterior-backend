d9f65e204e356325d0237e35d39c4cacca50eb6f
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-20 14:03:50 +0900

    feat: 꿀템 추천 게시판 서비스 구현

--- src/honeyitems/honeyitems.service.ts
+++ src/honeyitems/honeyitems.service.ts
@@ -1,4 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { HoneyItemDto } from './dto/honeyitem.dto';
+import { HoneyItem } from 'src/honeyitems/honeyitem.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { extractExtender } from 'src/lib/extractExtender';
+import { modifyFileNameIncludeExtender } from 'src/lib/modifyFileNameIncludeExtender';
 
 @Injectable()
-export class HoneyitemsService {}
+export class HoneyitemsService {
+  constructor(
+    @InjectRepository(HoneyItem)
+    private honeyItemRepository: Repository<HoneyItem>,
+  ) {}
+
+  /** 꿀템 게시판 글 전체 불러오기 */
+  async findAll(): Promise<HoneyItem[]> {
+    return await this.honeyItemRepository.find();
+  }
+
+  /** 꿀템 게시판 글 상세보기 */
+  async findOneById(id: number): Promise<HoneyItem> {
+    const found = await this.honeyItemRepository.findOneBy({ id });
+
+    if (!found) {
+      throw new NotFoundException('존재하지 않는 글입니다.');
+    }
+
+    return found;
+  }
+
+  /** 꿀템 게시판 글 작성 */
+  async createBoard(
+    honeyItemDto: HoneyItemDto,
+    file: Array<Express.Multer.File>,
+    user: User,
+  ): Promise<HoneyItem> {
+    const { title, description } = honeyItemDto;
+    let filename = '';
+
+    if (file.length > 0) {
+      const uploadFile = file[0];
+      const extender = extractExtender(uploadFile.mimetype);
+      modifyFileNameIncludeExtender(uploadFile.filename, extender);
+
+      filename = uploadFile.filename + '.' + extender;
+    }
+
+    const new_board = this.honeyItemRepository.create({
+      title,
+      description,
+      image: filename,
+      user,
+      authorId: user.id,
+    });
+
+    await this.honeyItemRepository.save(new_board);
+    return new_board;
+  }
+
+  /** 꿀템 추천 게시판 글 삭제 */
+  async deleteBoardById(
+    id: number,
+    user: User,
+  ): Promise<void | { message: string }> {
+    const found = await this.findOneById(id);
+
+    if (found.authorId === user.id) {
+      await this.honeyItemRepository.delete({ id });
+    } else {
+      return { message: '권한이 없습니다.' };
+    }
+  }
+
+  /** 꿀템 추천 게시판 글 수정 */
+  async updateBoardById(
+    id: number,
+    newDescription: string,
+    user: User,
+  ): Promise<HoneyItem | { message: string }> {
+    const found = await this.findOneById(id);
+
+    if (found.authorId === user.id) {
+      found.description = newDescription;
+      this.honeyItemRepository.save(found);
+
+      return found;
+    } else {
+      return { message: '권한이 없습니다.' };
+    }
+  }
+}
