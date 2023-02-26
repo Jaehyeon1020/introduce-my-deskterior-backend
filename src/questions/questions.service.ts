00bbb085125ae6773c8e8eda05fab210f465e4f2
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-26 17:27:41 +0900

    feat: 글 삭제할 때 AWS S3 업로드되어있는 이미지도 같이 삭제하도록 구현

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -89,9 +89,32 @@ export class QuestionsService {
   /** 질문 게시판 글 삭제 */
   async deleteBoardById(id: number, user: User): Promise<void> {
     const found = await this.findOneById(id);
+    const url = found.image.split('/');
+    const imageKey = url[url.length - 1];
 
     if (found.authorId === user.id) {
       await this.questionRepository.delete({ id });
+
+      // AWS 설정
+      AWS.config.update({
+        region: process.env.AWS_REGION,
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        },
+      });
+
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: imageKey,
+      };
+
+      await new AWS.S3().deleteObject(params, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('Deleted:', data);
+      });
     } else {
       throw new UnauthorizedException('권한이 없습니다.');
     }
